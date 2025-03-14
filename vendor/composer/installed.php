fbb8532c6cd5fed1a5503cb826c58e2445ae6818
HUỲNH QUỐC TIẾN <115643869+HuynhQuocTien@users.noreply.github.com>
2025-03-14 14:19:44 +0700

    add PSR logging interfaces and classes for improved logging functionality

---
+++ vendor/composer/installed.php
@@ -0,0 +1,47 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor/car-rental-website',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0c86e3105e4b2111442ec98da26c9ff5efa16918',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'monolog/monolog' => array(
+            'pretty_version' => '2.10.0',
+            'version' => '2.10.0.0',
+            'reference' => '5cf826f2991858b54d5c3809bee745560a1042a7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/log' => array(
+            'pretty_version' => '3.0.2',
+            'version' => '3.0.2.0',
+            'reference' => 'f16e1d5863e37f8d8c2a01719f5b34baa2b714d3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/log',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/log-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '1.0.0 || 2.0.0 || 3.0.0',
+            ),
+        ),
+        'vendor/car-rental-website' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0c86e3105e4b2111442ec98da26c9ff5efa16918',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
